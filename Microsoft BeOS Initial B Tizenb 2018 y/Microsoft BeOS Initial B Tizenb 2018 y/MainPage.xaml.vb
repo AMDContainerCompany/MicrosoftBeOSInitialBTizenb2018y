3239e1dbc51068129f2c21f318fdf258de1d92d8
NataliaVodianova <39686420+NataliaVodianova@users.noreply.github.com>
2018-05-29 06:25:24 +0900

    Internet DO
    RELEASE-F

---
+++ Microsoft BeOS Initial B Tizenb 2018 y/Microsoft BeOS Initial B Tizenb 2018 y/MainPage.xaml.vb
@@ -0,0 +1,38 @@
+﻿Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("81.200.1.4", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("800.80.24.900.Bzfs-ORD-JiIii-rx-JFs-001", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("801", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("821", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("824", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("827", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("826", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("824", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("823", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("801101010-SHRRR x-zJPHI", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("J", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("S  S    S h", EventLogEntryType.Warning, 506)
+
+Dim log As New EventLog("MyApplication3", "IBM VMs", "ZFS-S")
+log.WriteEntry("HRUTIC", EventLogEntryType.Warning, 506)
